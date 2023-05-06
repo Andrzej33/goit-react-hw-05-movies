f534683677b0fb386c6e20855b0dffc7127d469e
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-05-06 18:55:01 +0300

    add functions for fetches

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,33 +1,38 @@
 import axios from 'axios';
 
 const API_KEY = 'd1209b93bfca019c1d550689906b1b5c';
-const URL = 'https://api.themoviedb.org/3/trending/movie/day';
+const URL = 'https://api.themoviedb.org/3/';
+// const URL = 'https://api.themoviedb.org/3/trending/movie/day';
 
 // https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
 
 export const fetchTrendy = async () => {
-  const OPTIONS = new URLSearchParams({
-    api_key: API_KEY,
-    // query,
-    // media_type: 'movie',
-    // time_window: 'week',
-    // per_page: 20,
-  });
-  const ressponse = await axios(`${URL}?${OPTIONS}`);
+ 
+  const ressponse = await axios(`${URL}trending/movie/day?api_key=${API_KEY}`);
   return ressponse.data;
 };
 
 
 
 export const fetchById = async (movieId) => {
-  const ID_URL = `https://api.themoviedb.org/3/movie/${movieId}`;
-  const OPTIONS = new URLSearchParams({
-    api_key: API_KEY,
-    // query,
-    // media_type: 'movie',
-    // time_window: 'week',
-    // per_page: 20,
-  });
-  const ressponse = await axios(`${ID_URL}?${OPTIONS}`);
+  const ressponse = await axios(`${URL}movie/${movieId}?api_key=${API_KEY}`);
+  return ressponse.data;
+};
+
+
+export const fetchByQuery = async (query) => {
+  const ressponse = await axios(`${URL}search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`);
+  return ressponse.data;
+};
+
+
+export const fetchCreditsById = async (movieId) => {
+  const ressponse = await axios(`${URL}movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`);
+  return ressponse.data;
+};
+
+
+export const fetchReviewById = async (movieId) => {
+  const ressponse = await axios(`${URL}movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`);
   return ressponse.data;
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,17 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { StyledLink } from "./StyledLink/StyledLink";
 // import { useState, } from "react";
-import styled from "styled-components";
+
 import  {Home}  from "pages/Home";
 import  {Movies}  from "pages/Movies";
 import {MovieDetails} from "pages/MovieDetails";
 
 
-const StyledLink = styled(NavLink)`
-  color: black;
 
-  &.active {
-    color: orange;
-  }
-`;
 
 
 export const App = () => {
-// const [id, setId] = useState('')
 
-// const getId = movieId => {
-//   setId(movieId)
-  
-// }
-
-// useEffect(()=>{
-//  if(!id){
-//   return
-//  }
-//  const getMoviesById = async () => {
-//   try {
-//       const res = await fetchById(id); 
-//       console.log(res);
-//       // setMovies(results)  
-//   } catch (error) {
-//      console.log(error.message); 
-//   }
-// }
-// getMoviesById()
-// },[id])
 
   return (
     <div>
