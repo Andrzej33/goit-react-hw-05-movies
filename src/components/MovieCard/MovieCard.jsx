dfdc97990c3614abbacd2b5b61c3881c5c7ea969
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-05-08 18:32:08 +0300

    add check for the availability  data on movieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,22 +3,40 @@ import { Link } from "react-router-dom";
 export const MovieCard =({movie}) => {
 const {original_title,release_date,poster_path,vote_average,overview,genres} = movie;
 
-const imageUrl = `https://image.tmdb.org/t/p/w300${poster_path}`
+
+const release = release_date ? 
+`(${release_date.split("-")[0]})`:
+''
+;
+
+const roundedAverage = vote_average ? 
+`Use Score: ${Number((vote_average*10).toFixed(0))}%`
+ :
+'No Ranking';
+
+
+const imageUrl = poster_path? `https://image.tmdb.org/t/p/w400/${poster_path}`:
+'https://dummyimage.com/450x550/a3dae6/020430&text=Movie+and+poster+should+be+here...'
     return(
        <>
         <div>
         <Link to="/" end><button type="button">Go Back</button></Link>
             
             <div>
-<img src={imageUrl
-} alt="" />
+<img src={imageUrl} alt={original_title} />
 <div>
-    <h2>{original_title}<span>{release_date}</span></h2>
-    <p>Use Average:{vote_average}%</p>
-    <h3>Overview</h3>
+
+    <h2>{original_title ?? 'Unknown'}
+    {release && <span>{release}</span>}</h2>
+    <p>{roundedAverage}</p>
+    {overview && <>
+        <h3>Overview</h3>
     <p>{overview}</p>
-    <h3>Genres</h3>
-    {genres && <p>{genres.map(genre => genre.name).join(', ')}</p>}
+    </>}
+    
+    {genres.length > 0 && <>
+        <h3>Genres</h3> <p>{genres.map(genre => genre.name).join(', ')}</p>
+    </>}
 </div>
             </div>
         </div>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -14,7 +14,7 @@ useEffect(()=>{
   const getMovieById = async() =>{
     try {
       const res = await fetchById(movieId); 
-      console.log(res);
+      // console.log(res);
       setMovie(res)
     } catch (error) {
       console.log(error.message);
@@ -23,9 +23,6 @@ useEffect(()=>{
   getMovieById()
 },[movieId]);
 
-// const handleActors = () => {
-
-// }
 
 
 
